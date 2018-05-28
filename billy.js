de89ba5eebacb577bbb6bc30bed73d6cb1786e08
Cserkó József <j.cserko@gmail.com>
2018-05-28 21:55:51 +0200

    Update billy.js

--- billy.js
+++ billy.js
@@ -20,16 +20,16 @@ function billy(inp) {
     var temp = {};
 	var user = {};
     for (let k in inp) {
-		temp = {name: '', email: '', age: 0};
-		for (let j in inp[k]) {
-			if (/^[0-9]*$/.test(inp[k][j])) {
-				temp.age = inp[k][j];
+	temp = {name: '', email: '', age: 0};
+	for (let j in inp[k]) {
+	    if (/^[0-9]*$/.test(inp[k][j])) {
+		temp.age = inp[k][j];
             } else if (/@/.test(inp[k][j])) {
-				temp.email = inp[k][j];
+		temp.email = inp[k][j];
             } else {
-				temp.name = inp[k][j];
+		temp.name = inp[k][j];
             }
-		}
+	}
         
         if (temp.email == 'billy@gmail.com') {
             return temp;
